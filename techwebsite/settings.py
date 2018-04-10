a6cc1855f63e902bfc465e49e98937016a1aae86
vishvesh <kapadia.vishvesh@gmail.com>
2018-04-10 12:05:36 +0530

    initial

--- techwebsite/settings.py
+++ techwebsite/settings.py
@@ -75,12 +75,13 @@ WSGI_APPLICATION = 'techwebsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'djangostack',
-        'HOST': '/opt/bitnami/postgresql',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'techwebsite',
+        'USER': 'postgres',
+        'PASSWORD': '0qLJWEUlU0jg',
+        'HOST': 'localhost',
         'PORT': '5432',
-        'USER': 'bitnami',
-        'PASSWORD': '69378ef93a'
+
     }
 }
 SUMMERNOTE_CONFIG = {
